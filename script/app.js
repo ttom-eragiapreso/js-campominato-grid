abec98fd45621e9a3b9d64194e2efae631560315
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2022-10-20 16:39:07 +0200

    aggiunto difficoltà e reset

--- script/app.js
+++ script/app.js
@@ -12,31 +12,78 @@ console.log("app loaded")
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
 
+
+// Mi prendo gli elementi del DOM con i quali dovrò interagire
 const container = document.querySelector(".container");
+const button = document.querySelector("#inizia");
+const select = document.querySelector("#selectDifficoltà");
+const reset = document.getElementById("reset");
 
-const button = document.querySelector("button");
 
+
+// Aggiungo una funzione custom al click del bottone per iniziare
 button.addEventListener("click", createSquares);
 
 
+let isEmpty = true;
+
+// La funzione custom
 function createSquares(){
+
+  // Appena clicco controllo subito se non ho cliccato prima con questa flag
+  if(isEmpty){
+
+    // Per prima cosa rimuovo la classe che rende invisibile il bottone di reset
+    reset.classList.remove("hide");
+    // Poi inizializzo una variabile che prenderà in conto la difficoltà scelta dall'utente
+    let difficoltà;
   
-  for(let i = 1; i < 101; i++){
-    let square = document.createElement("div");
-    square.classList.add("square");
-    square.innerHTML = i;
-    container.append(square);
-    square.addEventListener("click", function(){
-      console.log(this.innerHTML);
-      this.classList.add("blue")
-    })
+    // Faccio un switch per comparare il valore del select al momento del click e vado a scrivere dentro difficoltà il limite massimo di quadrati che poi andrò a scrivere
+    switch(select.value){
+  
+      case "2":
+        difficoltà = 82;
+        console.log(difficoltà, select.value)
+        break;
+        
+      case "3":
+        difficoltà = 50;
+        console.log(difficoltà, select.value)
+        break
+  
+      default:
+        difficoltà = 101;
+        console.log(difficoltà, select.value)
+        break
+  }
+    
+    // Faccio un loop da 1 fino a il valore trovato con lo switch e per ogni iterazione creo un elemento div, con classe square, che dentro ha il numero al contatore del loop, e infine ad esso stesso gli creo un addEventListener che al click gli assegni una classe per colorarlo e stampi in console il suo numero.
+    for(let i = 1; i < difficoltà; i++){
+      let square = document.createElement("div");
+      square.classList.add("square");
+      square.innerHTML = i;
+      container.append(square);
+      square.addEventListener("click", function(){
+        console.log(this.innerHTML);
+        this.classList.add("blue")
+      })
+    }
+    // Una volta che finisce tutta la logica del click inverto il valore della flag.
+    isEmpty = false;
+  }else {
+
+    console.log("Devi prima resettare")
   }
   
-
 }
 
 
-
+// La funzione di reset che avviene quando il bottone di reset viene premuto semplicemente cancella tutto quello che c'è dentro al container che ospita tutti i quadrati. Infine nasconde se stesso.
+reset.addEventListener("click", function(){
+  container.innerHTML = "";
+  this.classList.add("hide");
+  isEmpty = true;
+})
 
 
 
